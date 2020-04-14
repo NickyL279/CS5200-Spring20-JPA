a80e24bbbe802bbac7aad28b3927dc55d97e6b45
yrrah <yrrah7@gmail.com>
2020-04-14 12:02:23 -0400

    added CORS setting

--- cs5200-spring2020-hartenstine-master/src/main/java/edu/northeastern/cs5200/Cs5200Spring2020HartenstineApplication.java
+++ cs5200-spring2020-hartenstine-master/src/main/java/edu/northeastern/cs5200/Cs5200Spring2020HartenstineApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //import org.springframework.boot.web.support.SpringBootServletInitializer;
 //if you face an error with above imports, try
@@ -23,6 +26,16 @@ public class Cs5200Spring2020HartenstineApplication
   public static void main(String[] args) {
     SpringApplication.run(
             Cs5200Spring2020HartenstineApplication.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") .allowedOrigins("https://shrouded-dusk-60647.herokuapp.com/", "http://localhost:3000");
+      }
+    };
   }
 
 }
