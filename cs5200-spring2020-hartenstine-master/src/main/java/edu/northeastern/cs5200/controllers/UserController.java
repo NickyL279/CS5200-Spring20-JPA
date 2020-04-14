7b0605c981c500293704397eaed450279b101a6e
yrrah <yrrah7@gmail.com>
2020-04-14 12:10:41 -0400

    added CORS setting

---
+++ cs5200-spring2020-hartenstine-master/src/main/java/edu/northeastern/cs5200/controllers/UserController.java
@@ -0,0 +1,30 @@
+package edu.northeastern.cs5200.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import edu.northeastern.cs5200.models.users.User;
+import edu.northeastern.cs5200.repositories.UserRepository;
+
+@RestController
+public class UserController {
+
+  @Autowired
+  UserRepository userRepository;
+
+  @GetMapping("/api/allUsers")
+  public List<User> allUsers() {
+    return (List<User>) userRepository.findAll();
+  }
+
+  @GetMapping("/api/addUser")
+  public String addUser() {
+    userRepository.save(new User("alice", "wonder", "username", "pass"));
+    return "done";
+  }
+
+}
--- cs5200-spring2020-hartenstine-master/src/main/java/edu/northeastern/cs5200/daos/JobSearchDao.java
+++ cs5200-spring2020-hartenstine-master/src/main/java/edu/northeastern/cs5200/daos/JobSearchDao.java
@@ -1,24 +1,25 @@
-package edu.northeastern.cs5200;
+package edu.northeastern.cs5200.daos;
 
-import edu.northeastern.cs5200.models.users.*;
-import edu.northeastern.cs5200.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import edu.northeastern.cs5200.models.users.Advisor;
+import edu.northeastern.cs5200.models.users.Applicant;
+import edu.northeastern.cs5200.models.users.User;
+import edu.northeastern.cs5200.repositories.AdvisorRepository;
+import edu.northeastern.cs5200.repositories.ApplicantRepository;
+import edu.northeastern.cs5200.repositories.FavoriteRepository;
+import edu.northeastern.cs5200.repositories.JobRepository;
+import edu.northeastern.cs5200.repositories.UserRepository;
+
 @Component
-public class JobSearchDaoImpl implements JobSearchDao {
+public class JobSearchDao {
 
   @Autowired
   private UserRepository userRepository;
 
   @Autowired
   private AdvisorRepository advisorRepository;
-
-  @Autowired
-  private AdminRepository adminRepository;
-
-  @Autowired
-  private StudentRepository studentRepository;
 
   @Autowired
   private ApplicantRepository applicantRepository;
@@ -29,43 +30,22 @@ public class JobSearchDaoImpl implements JobSearchDao {
   @Autowired
   private JobRepository jobRepository;
 
-  @Override
   public void truncateDatabase() {
     favoriteRepository.deleteAll();
     userRepository.deleteAll();
     jobRepository.deleteAll();
-    advisorRepository.deleteAll();
-    adminRepository.deleteAll();
-    applicantRepository.deleteAll();
-    favoriteRepository.deleteAll();
-
   }
 
-  @Override
   public User createUser(User user) {
     userRepository.save(user);
     return user;
   }
 
-  @Override
   public Advisor createAdvisor(Advisor advisor) {
     advisorRepository.save(advisor);
     return advisor;
   }
 
-  @Override
-  public Admin createAdmin(Admin admin) {
-    adminRepository.save(admin);
-    return admin;
-  }
-
-  @Override
-  public Student createStudent(Student student) {
-    studentRepository.save(student);
-    return student;
-  }
-
-  @Override
   public Applicant createApplicant(Applicant applicant) {
     applicantRepository.save(applicant);
     return applicant;
